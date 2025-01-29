498a8f49d5aec383f7386e3f0bee2085c4236930
DESKTOP-6VVQ9J9\LisikMe <Lisikme>
2025-01-29 21:05:32 +0300

    78787878

--- app.py
+++ app.py
@@ -298,15 +298,15 @@ if not os.path.exists(f'./{DIR}'):
     os.makedirs(f'./{DIR}')
 
 # Изменение каталога
-Строка = 79
+Строка = 87
 with open('./index.html', 'r', encoding='utf8') as Файл:
     lines = Файл.readlines()
 lines.insert(Строка-2, f'''
 <section class="hero1"><div class="container"><div class="hero-inner"><div class="hero-copy"><div class="film"><h1 class="items">
     <span class="item1">{Имя}</span><p>
     <span class="item2">{Возраст}</span></p></h1></div>
-    <div class="im"><img src="pages/{num}/image.png" class="image" loading="lazy" onerror="this.style.visibility = 'hidden'" width="170px" height = "250px"><div class="hero-cta">
-    <a class="buttonn" href="?id={num}">Смотреть</a>
+    <div class="im"><img src="./pages/{num}/image.png" class="image" loading="lazy" onerror="this.style.visibility = 'hidden'" width="170px" height = "250px"><div class="hero-cta">
+    <a class="buttonn" href="./pages/{num}">Смотреть</a>
 </section>
 ''')
 Файл = open('./index.html', 'w+', encoding='utf8')
