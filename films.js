02de55cf153baaf7eb8b69db28b0dc8746de1c61
DESKTOP-6VVQ9J9\LisikMe <Lisikme>
2025-01-31 00:05:49 +0300

    888

---
+++ films.js
@@ -0,0 +1,17 @@
+const FilmsList = [
+    {
+        'Title': 'Головоломка 3',
+        'AgeLimit': '16+',
+        'VideoUrl': 'https://polynoy-as.newplayjj.com/3'
+    },
+    {
+        'Title': 'Головоломка 2',
+        'AgeLimit': '6+',
+        'VideoUrl': 'https://polynoy-as.newplayjj.com/2'
+    },
+    {
+        'Title': 'Головоломка 1',
+        'AgeLimit': '0+',
+        'VideoUrl': 'https://polynoy-as.newplayjj.com/1'
+    }
+];
